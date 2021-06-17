77a7fd4b8583ef226c5e8c0db9373fd538238bf8
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-17 19:01:33 +0300

    use of for loop in java

---
+++ 1.Beginner Java/Chapter~1/Lesson 7: For loop.java
@@ -0,0 +1,20 @@
+
+    // FOR LOOPS:
+    // used for executing repetitive tasks
+    // declare counter loop varibale(initialization), how many times to be executed, increment/decrement
+
+    // print with parenthesis
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+    System.out.println("hello world 5 times");
+
+    // loop incrementing 
+    for (int a = 1; a<5; a++)
+      System.out.println("Loop at number: " + a);
+
+    // decrementing
+    for (int i = 5; i>0; i--){
+      System.out.println("decrementing values, current number: " + i);
+    }
